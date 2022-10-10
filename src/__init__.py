80c9697a86441cff1fe722a5d1b003e3d4108637
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-10-10 19:16:12 -0300

    feat: add MongoDB

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import os
 
 app = Flask(__name__)
+
+# Setup MongoDB
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+client = PyMongo(app)
+database = client.db
+print(database)
 
 from src import routes
